d5c8eef3a1174864706be7e5f0dd9fcfc139361f
Vladislav Semenov <arctic.ratatat@gmail.com>
2022-01-16 23:51:57 +0300

    drop test

--- src/test/java/ru/netology/web/TestOrderForm.java
+++ src/test/java/ru/netology/web/TestOrderForm.java
@@ -1,18 +1,21 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestOrderForm {
+    @Test
     void shouldAccepted() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Виталий Петров-Белых");
+        $("[data-test-id=name] input").setValue("John Williams");
         $("[data-test-id=phone] input").setValue("+79998338383");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно " +
-                "отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
 
